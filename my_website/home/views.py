8156395c6a4ac01f1183f7a26fade33461206065
touir1 <touir.mat@gmail.com>
2018-06-11 18:44:41 +0100

    structuring the project + testings views and urls

--- my_website/home/views.py
+++ my_website/home/views.py
@@ -4,5 +4,6 @@ from django.shortcuts import render
 def home(request):
     return HttpResponse("""
         <h1>Welcome to my website ! </h1>
-        <p>this is from the blog home view</p>
+        <p>it's only a test of HttpResponse from the views.py file</p>
+        <b>It's from the home :D</b>
     """)
